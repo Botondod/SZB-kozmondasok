fc019a159de2e9a7d6bcc5e824a9f96a75303745
Botondod <szabo.botond@diak.szbi-pg.hu>
2025-01-31 07:29:12 +0100

    Add project files.

---
+++ SZB-kozmondasok/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZB_kozmondasok
+{
+    internal class Program
+    {
+        static void asd ()
+        {
+            List<string> lista = new List<string>();
+            List<string[]> listab = new List<string[]>();
+
+            StreamReader be = new StreamReader("szoveg1.txt");
+            StreamReader bee = new StreamReader("szoveg2.txt");
+            
+            string sor;
+            string[] reszek;
+
+            sor = be.ReadLine();
+            while (sor != null) 
+            {
+                reszek = sor.Split('.');
+                lista.Add(reszek[0]);
+                sor = be.ReadLine();
+            }
+            be.Close();
+
+
+
+
+            while (sor != null)
+            {
+                reszek = sor.Split('.');
+                listab.Add(reszek[0]);
+                sor = bee.ReadLine();
+            }
+            bee.Close();
+        }
+        static void Main(string[] args)
+        {
+            /* 
+            Közmondasok
+            SZB - 2025.01.28.
+            */
+            string fejlec = "Közmondasok";
+            Console.WriteLine(fejlec);
+            for (int i = 0; i < fejlec.Length; i++) Console.Write('-');
+            Console.WriteLine();
+
+            
+
+
+
+
+            Console.WriteLine("Nyomj meg bármit a kilépéshez!");
+            Console.ReadKey();
+        }
+    }
+}
